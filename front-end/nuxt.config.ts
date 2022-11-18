47a215a14833e4a6a435bf128cc383ab1cacb03b
ShoContain <perblue.666@gmail.com>
2022-11-19 03:55:44 +0900

    feat:  cmsのclient定義

--- front-end/nuxt.config.ts
+++ front-end/nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  runtimeConfig: {
+    apiKey: process.env.API_KEY || '',
+    serviceDomain: process.env.SERVICE_DOMAIN || ''
+  }
 })
